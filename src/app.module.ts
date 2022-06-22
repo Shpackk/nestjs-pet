59da2e3a196f045c3e498c81ad4acc5e3cb63d42
Shpackk <victorija45@gmail.com>
2022-06-22 18:24:54 +0300

    feat: add new modules, controllers, providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ProductsModule, UserModule],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
