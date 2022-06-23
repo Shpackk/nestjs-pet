92faa10ac16b5133702bcf7c3445630e2d1bee59
Shpackk <victorija45@gmail.com>
2022-06-23 16:33:06 +0300

    fix: better naming for login route dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserLoginDto } from './dto/userLogin.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('user')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login(@Body() userLoginDto: UserLoginDto): void {
+  login(@Body() userLoginDto: LoginDto): void {
     this.authService.login(userLoginDto);
     //return success if login made it
   }
