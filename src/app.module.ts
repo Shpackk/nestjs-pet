6c6f396cf5cd32dd6ebcc81067abdf7609daa990
Shpackk <victorija45@gmail.com>
2022-06-23 16:28:46 +0300

    feat: register providers in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ProductsService } from './products/products.service';
 import { ProductsModule } from './products/products.module';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [AuthModule, ProductsModule, UserModule],
   controllers: [AppController, AuthController, UserController],
-  providers: [AppService, ProductsService],
+  providers: [AppService, ProductsService, AuthService, UserService],
 })
 export class AppModule {}
