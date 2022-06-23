9ed845178233808746e0bbd35382294bdc876f4d
Shpackk <victorija45@gmail.com>
2022-06-23 16:32:41 +0300

    fix: better naming for login route dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/interfaces/user.interface';
-import { UserLoginDto } from './dto/userLogin.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   private readonly users: User[] = [];
-  login(userLoginDto: UserLoginDto) {
+  login(userLoginDto: LoginDto) {
     console.log(userLoginDto);
     //check in db, and gen token
   }
