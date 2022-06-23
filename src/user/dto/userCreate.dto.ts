48e98ee91e8008f94a079a8432c45f46c0dbd390
Shpackk <victorija45@gmail.com>
2022-06-23 16:29:31 +0300

    feat: add mapped type for user deletion

--- src/user/dto/userCreate.dto.ts
+++ src/user/dto/userCreate.dto.ts
@@ -1,5 +1,8 @@
+import { PickType } from '@nestjs/mapped-types';
 export class UserDto {
   name: string;
   login: string;
   location: string;
 }
+
+export class DeleteUserDto extends PickType(UserDto, ['login'] as const) {}
