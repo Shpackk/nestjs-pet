ee8cf5b0f4507a284542d2b36a0b61ffcab9fc1f
Shpackk <victorija45@gmail.com>
2022-06-24 19:04:02 +0300

    feat: add validation of fields

--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -1,4 +1,10 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class ProductDto {
+  @IsString()
   name: string;
+  @IsNumber()
   amount: number;
+  @IsNumber()
+  price: number;
 }
