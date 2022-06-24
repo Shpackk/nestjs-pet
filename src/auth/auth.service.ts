ee7c7f22248d00e2a69a9c7432f42655af2530d0
Shpackk <victorija45@gmail.com>
2022-06-24 19:02:41 +0300

    change name of login dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/interfaces/user.interface';
+import { UserInterface } from 'src/interfaces/user.interface';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  private readonly users: User[] = [];
-  login(userLoginDto: LoginDto) {
-    console.log(userLoginDto);
+  private readonly users: UserInterface[] = [];
+  login(loginDto: LoginDto) {
+    console.log(loginDto);
     //check in db, and gen token
   }
 }
