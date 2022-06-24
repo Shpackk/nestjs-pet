add0507f089db9344ae1bd65c67ae5c67d688ec9
Shpackk <victorija45@gmail.com>
2022-06-24 19:08:46 +0300

    feat: mongo connection, bcrypt service injection

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BcryptService } from 'src/services/bcrypt.service';
+import { User, UserSchema } from './schemas/user.schema';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService, BcryptService],
 })
 export class UserModule {}
