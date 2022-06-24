65f847d06d94284c5b198590e3d394bf420d13a0
Shpackk <victorija45@gmail.com>
2022-06-24 19:03:35 +0300

    fix: user interface name

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,9 +1,5 @@
-export class User {
+export class UserInterface {
   name: string;
   login: string;
   location: string;
-  request?: string;
-  //current request of user which is not fullfilled
-  historyOfRequests?: string;
-  //historyOfRequests should be type of request[]
 }
