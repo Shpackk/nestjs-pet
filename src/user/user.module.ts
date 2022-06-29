699c8aa99d29e5e76974106ea93522c1cb2ae682
Shpackk <victorija45@gmail.com>
2022-06-29 19:27:51 +0300

    feat: add userRepository to providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/entities/user.entity';
+import { UserRepository } from 'src/database/repositories/user.repo';
 import { BcryptService } from 'src/services/bcrypt.service';
-import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService, BcryptService],
+  providers: [UserService, BcryptService, UserRepository],
 })
 export class UserModule {}
