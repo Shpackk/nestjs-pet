69e13ea9039cdf45702aa2cf16a2256b6b5b73a3
Shpackk <victorija45@gmail.com>
2022-06-29 19:26:19 +0300

    fix: add spacing

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -2,10 +2,13 @@ import { IsString } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   name: string;
+
   @IsString()
   login: string;
+
   @IsString()
   location: string;
+
   @IsString()
   password: string;
 }
