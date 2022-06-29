7c1ebe25760ddc03f52498ce2261eaedfbdf1786
Shpackk <victorija45@gmail.com>
2022-06-29 19:26:35 +0300

    fix: add spacing

--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -3,8 +3,10 @@ import { IsNumber, IsString } from 'class-validator';
 export class ProductDto {
   @IsString()
   name: string;
+
   @IsNumber()
   amount: number;
+
   @IsNumber()
   price: number;
 }
