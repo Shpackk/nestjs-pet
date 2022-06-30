ba98a60f7bf4a93aaeb8c50d57707d1ac53e6cab
Shpackk <victorija45@gmail.com>
2022-06-30 19:41:12 +0300

    feat: inject related repo to module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductRepository } from 'src/database/repositories/product.repo';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductRepository],
 })
 export class ProductsModule {}
