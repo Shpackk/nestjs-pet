a9d73d83de9d7ce40803063263bccd7f6b0253ab
Shpackk <victorija45@gmail.com>
2022-06-30 19:42:20 +0300

    fix: structure, omit code duplication

--- src/user/dto/dbUser.dto.ts
+++ src/user/dto/dbUser.dto.ts
@@ -1,4 +1,6 @@
-export class DbUserDto {
-  name: string;
-  location: string;
+import { OmitType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './createUser.dto';
+
+export class DbUserDto extends OmitType(CreateUserDto, ['password'] as const) {
+  hashPassword: string;
 }
