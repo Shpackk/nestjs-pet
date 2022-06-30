f27471c704fa04de61fa11c109681c961bc0aae0
Shpackk <victorija45@gmail.com>
2022-06-30 19:40:44 +0300

    feat: controllers now support work with queries

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,23 +1,23 @@
 import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
-import { Product } from 'src/interfaces/product.interface';
+import { ProductEntity } from 'src/database/entities/product.entity';
+import { UpdateResult } from 'typeorm';
 import { ProductDto } from './dto/product.dto';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
-  @Get('all')
-  findAll(): Product[] {
-    //should return Promise<Product[]>
-    return this.productsService.findAll();
-  }
   @Post('add')
-  addProducts(@Body() productDto: ProductDto): void {
-    this.productsService.add(productDto);
+  async addProducts(@Body() productDto: ProductDto): Promise<ProductEntity> {
+    return await this.productsService.add(productDto);
+  }
+  @Get('all')
+  async findAll(): Promise<ProductEntity[]> {
+    return await this.productsService.findAll();
   }
   @Patch('update')
-  updateProducts(@Body() productDto: ProductDto): Product[] {
-    return this.productsService.update(productDto);
+  async updateProduct(@Body() productDto: ProductDto): Promise<UpdateResult> {
+    return await this.productsService.update(productDto);
     //shoould return Promise<Product[]>
   }
 }
