4c02af518a739eeb3e5c5733fdfc9cd024a713ab
Shpackk <victorija45@gmail.com>
2022-07-04 20:11:56 +0300

    fix: import typeorm module/ product entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from 'src/database/entities/product.entity';
 import { ProductRepository } from 'src/database/repositories/product.repo';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [ProductsController],
   providers: [ProductsService, ProductRepository],
 })
